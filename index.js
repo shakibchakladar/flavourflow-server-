240881c560ef78a79397f9f54e22f5838f47959b
shakibchakladar <shakibchakladar@gmail.com>
2024-05-14 01:21:32 +0600

    delete operation in purchase

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete("/purchase/:id",async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await purchaseCollection.deleteOne(query)
+      res.send(result);
+
+    })
+
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
