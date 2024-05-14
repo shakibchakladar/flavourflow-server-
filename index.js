c166a2f4ed56f14778910a7d936634086bea3370
shakibchakladar <shakibchakladar@gmail.com>
2024-05-15 01:45:15 +0600

    implement jwt token

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const jwt=require('jsonwebtoken')
+const cookieParser=require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app=express();
 const port=process.env.PORT ||5000;
 
 // midleware
-app.use(cors());
+app.use(cors({
+  origin:[
+    'http://localhost:5173',
+    'https://flavorflow-6eaef.web.app',
+    'https://flavorflow-6eaef.firebaseapp.com'
+  ],
+  credentials:true,
+  // optionsSuccessStatus:200,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9ttivus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,6 +31,27 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares
+const logger=(req,res,next)=>{
+  console.log('log:info', req.method,req.url);
+  next()
+}
+
+const verifyToken=(req,res,next)=>{
+  const token=req?.cookies?.token;
+  // console.log('token in the midleware',token);
+  if(!token){
+    return res.status(401).send({message: 'unauthorise acces'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'unauthorised acces'})
+    }
+    req.user=decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -29,9 +61,33 @@ async function run() {
     const foodCollection=client.db('flavorflow').collection('foods');
     const purchaseCollection=client.db('flavorflow').collection('purchase')
 
+    // auth related api
+    app.post("/jwt",logger, async(req,res)=>{
+      const user=req.body;
+      console.log('user for token',user);
+      const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
+      res.cookie('token',token,{
+        httpOnly:true,
+        secure:true,
+        sameSite: 'none'
+      })
+      .send({success: true})
+    })
 
-    app.get("/myfood/:email",async(req,res)=>{
+    app.post("/logout",async(req,res)=>{
+      const user=req.body;
+      console.log('logging out',user);
+      res.clearCookie('token',{maxAge: 0}).send({success: true})
+    })
+
+
+    // food
+    app.get("/myfood/:email",logger,verifyToken, async(req,res)=>{
       // console.log(req.params.email);
+      console.log('token owner info',req.user);
+      if(req.user.email !== req.params.email){
+        return res.status(403).send({message: 'forbidden acces'})
+      }
       const result=await foodCollection.find({email:req.params.email}).toArray()
       res.send(result)
 
@@ -87,7 +143,7 @@ async function run() {
 
     
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
